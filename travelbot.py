ee6e91efc83035e871c595601dbb183e212cd7c5
harshalsakthirs-lgtm <harshalsakthirs@gmail.com>
2025-10-14 18:05:04 +0530

    Add files via upload

---
+++ travelbot.py
@@ -0,0 +1,51 @@
+import re, random
+from colorama import Fore, init
+from textblob.en import suggest
+
+init(autoreset=True)
+
+destinations = {
+    "beaches: " : ["Bali", "Maldives", "Phuket"],
+    "mountains: " : ["Swiss Alps", "Rocky Mountains", "Himalayas"],
+    "cities: " : ["Tokyo", "Paris", "New York"]
+}
+
+jokes = [
+    "Why still programmers, not like nature? because having bug",
+    "Why do the computer did go to the doctor? because it had a virus",
+    "Why do travellers feel warm? because all their hot spots"
+]
+
+def normalize_input(text):
+    return re.sub(r"\s+", "", text.strip().lower())
+
+def recommend():
+    print(f"{Fore.CYAN} Travel Bot: beaches or mountains or cities?")
+    preference = input(f"{Fore.GREEN} You: ")
+    preference = normalize_input(preference)
+
+    if preference in destinations:
+        suggestions = random.choice(destinations[preference])
+        print(f"{Fore.GREEN} Travel Bot: How about {suggestions}?")
+        print(f"{Fore.CYAN} Travel Bot: Did you like it: (yes/no): ")
+        answer = input(f"{Fore.YELLOW} You: ").lower()
+
+        if answer == "yes":
+            print(f"{Fore.GREEN} Travel Bot: Awesome, Enjoy!")
+        elif answer == "no":
+            print(f"{Fore.RED} Travel Bot: Let's try another!")
+            recommend()
+        else:
+            print(f"{Fore.RED} Sorry, I'll suggest another!")
+
+    else:
+        print(f"{Fore.RED}Travel Bot: Sorry, I not have that destinations")
+    show_help()
+
+def packing_tips():
+    print(f"{Fore.CYAN}Travel Bot: Where to?: ")
+    location = normalize_input(f"{Fore.YELLOW} You: ")
+    print(f"{Fore.CYAN}Travel Bot: How many days?: ")
+    days = input(f"{Fore.YELLOW}Travel Bot: You: ")
+
+    print(Fore.GREEN f"")
