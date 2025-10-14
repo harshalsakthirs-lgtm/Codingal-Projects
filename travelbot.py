487c6f00bf3cd4750668944341603fa718b2d49c
harshalsakthirs-lgtm <harshalsakthirs@gmail.com>
2025-10-14 18:27:49 +0530

    Add files via upload

--- travelbot.py
+++ travelbot.py
@@ -48,4 +48,45 @@ def packing_tips():
     print(f"{Fore.CYAN}Travel Bot: How many days?: ")
     days = input(f"{Fore.YELLOW}Travel Bot: You: ")
 
-    print(Fore.GREEN f"")
+    print(Fore.GREEN, f"Travel Bot: Packing tips for {days} days in {location}")
+    print(Fore.GREEN, f"Travel Bot: - Pack versatile cloths")
+    print(Fore.GREEN, f"Tral Bot: - Bring chargers/adapters")
+    print(Fore.GREEN, f"Travel Bot: - Chek the weather forecast")
+
+def tell_joke():
+     print(Fore.YELLOW + f"Travel Bot:{random.choice(jokes)}")
+def show_help():
+    print(Fore.MAGENTA + f"Travel Bot: \n I can:")
+    print(Fore.GREEN + "Travel Bot: - Suggests travel spots: (say 'recommend')")
+    print(Fore.GREEN + "- Offer packing tips (say 'packing' or 'pack')")
+    print(Fore.GREEN + "- Tell a joke (say 'joke')")
+    print(Fore.CYAN + "Type 'exit' or 'bye' to exit the program \n")
+
+def chat():
+    print(f"{Fore.CYAN} Hello I am Travel Bot!")
+    name = input(f"{Fore.YELLOW}What is your name?: ")
+    print(f"{Fore.CYAN}Hello Nice to meet you, {name}!")
+    show_help()
+
+    while True:
+        user_input = input(f"{Fore.YELLOW} {name}: ")
+        user_input = normalize_input(user_input)
+
+        if "recommend" in user_input:
+            recommend()
+        elif "packing" in user_input or "pack" in user_input:
+            packing_tips()
+        elif "joke" in user_input:
+            tell_joke()
+        elif "help" in user_input:
+            show_help()
+        elif "exit" in user_input or "bye" in user_input:
+            print(f"{Fore.CYAN} Safe Travel Good Bye!")
+            break
+        else:
+            print(f"{Fore.RED} Could you rephrase")
+
+if __name__ == "__main__":
+    chat()
+
+
